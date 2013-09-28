c511bce70600b98d8c9b0765e672b1e0cdfeeec1
Tres Seaver <tseaver@palladion.com>
2013-09-28 13:08:47 -0400

    Work toward creating profile on first login.

--- src/yss/yss/views.py
+++ src/yss/yss/views.py
@@ -1,8 +1,18 @@
 import base64
 import uuid
 
+import deform
+from pyramid.httpexceptions import HTTPFound
 from pyramid.view import view_config
+from pyramid.security import remember
+from substanced.interfaces import IUser
+from substanced.interfaces import IUserLocator
+from substanced.principal import DefaultUserLocator
+from substanced.util import find_service
+from substanced.util import get_oid
 from velruse import login_url
+
+from .resources import YSSProfileSchema
 
 @view_config(renderer="templates/home.pt")
 def home(request):
@@ -34,16 +44,34 @@ def generate_performance_id(performances):
 
 @view_config(
     context='velruse.AuthenticationComplete',
-    renderer='templates/logged_in.pt',
 )
 def login_complete_view(context, request):
-    return {
-        'ok': True,
-        'provider_type': context.provider_type,
-        'provider_name': context.provider_name,
-        'profile': context.profile,
-        'credentials': context.credentials,
-    }
+    provider = context.provider_name
+    profile = context.profile
+    username = profile['accounts'][0]['username']
+    root = request.virtual_root
+    adapter = request.registry.queryMultiAdapter(
+        (root, request), IUserLocator)
+    if adapter is None:
+        adapter = DefaultUserLocator(root, request)
+    user = adapter.get_user_by_login(username)
+    if user is None:
+        principals = find_service(request.virtual_root, 'principals')
+        user = principals.add_user(username, registry=request.registry)
+        user.display_name = profile['displayName']
+        addresses = profile.get('addresses')
+        if addresses:
+            user.email = addresses[0]['formatted']
+        photos = profile.get('photos')
+        if photos:
+            user.photo_url = photos[0]['value']
+        user.age = user.sex = user.favorite_genre = None
+        location = request.resource_url(user, 'edit.html')
+    else:
+        location = request.resource_url(user)
+    headers = remember(request, get_oid(user))
+    return HTTPFound(location, headers=headers)
+
 
 @view_config(
     context='velruse.AuthenticationDenied',
@@ -54,4 +82,46 @@ def login_denied_view(context, request):
         'ok': False,
         'provider_name': context.provider_name,
         'reason': context.reason,
+    }
+
+
+@view_config(
+    context=IUser,
+    renderer='templates/profile.pt',
+)
+def profile_view(context, request):
+    appstruct = {
+        'username': context.__name__,
+        'display_name': getattr(context, 'display_name', ''),
+        'email': getattr(context, 'email', ''),
+        'photo_url': getattr(context, 'photo_url', ''),
+        'age': getattr(context, 'age', 50),
+        'sex': getattr(context, 'sex', None),
+        'favorite_genre': getattr(context, 'favorite_genre', None),
+    }
+    form = deform.Form(YSSProfileSchema(), buttons=('Save',))
+    return {
+        'form': form.render(appstruct, readonly=True),
+    }
+
+
+@view_config(
+    context=IUser,
+    renderer='templates/profile.pt',
+    name='edit.html',
+#    permission='edit', XXX
+)
+def profile_edit(context, request):
+    appstruct = {
+        'username': context.__name__,
+        'display_name': getattr(context, 'display_name', ''),
+        'email': getattr(context, 'email', ''),
+        'photo_url': getattr(context, 'photo_url', ''),
+        'age': getattr(context, 'age', 50),
+        'sex': getattr(context, 'sex', None),
+        'favorite_genre': getattr(context, 'favorite_genre', None),
+    }
+    form = deform.Form(YSSProfileSchema(), buttons=('Save',))
+    return {
+        'form': form.render(appstruct, readonly=False),
     }
