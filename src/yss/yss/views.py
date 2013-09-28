947bce811f255aeddfafc7763c5850d3c1f40d5c
Chris McDonough <chrism@plope.com>
2013-09-28 11:05:11 -0400

    add a temporary main template and a favicon

--- src/yss/yss/views.py
+++ src/yss/yss/views.py
@@ -1,8 +1,8 @@
 from pyramid.view import view_config
 from velruse import login_url
 
-@view_config(renderer="templates/splash.pt"
-            )
-def itworks(request):
-    return {'twitter_login_url': login_url(request, 'twitter'),
-           }
+@view_config(renderer="templates/home.pt")
+def home(request):
+    return {
+        'twitter_login_url': login_url(request, 'twitter'),
+        }
