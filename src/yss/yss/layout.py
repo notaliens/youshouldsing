f479c0fa189dc2d0212229217df455c224f15043
Chris Rossi <chris@archimedeanco.com>
2013-09-28 10:52:00 -0400

    Use pyramid_layout

---
+++ src/yss/yss/layout.py
@@ -0,0 +1,10 @@
+from pyramid_layout.layout import layout_config
+
+
+@layout_config(template="templates/main_layout.pt")
+class MainLayout(object):
+    page_title = "You should sing!"
+
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
--- src/yss/yss/views.py
+++ src/yss/yss/views.py
@@ -1,8 +1,34 @@
+import base64
+import uuid
+
 from pyramid.view import view_config
 from velruse import login_url
+
 
 @view_config(renderer="templates/splash.pt"
             )
 def itworks(request):
     return {'twitter_login_url': login_url(request, 'twitter'),
            }
+
+
+@view_config(name="record",
+             renderer="templates/record.pt")
+def recording_app(request):
+    performance_id = generate_performance_id({})
+    return {
+        "id": performance_id,
+    }
+
+
+@view_config(name="record", xhr=True)
+def save_recording(request):
+    pass
+
+
+def generate_performance_id(performances):
+    while True:
+        id = base64.b64encode(uuid.uuid4().bytes).rstrip("==")[-8:]
+        if id not in performances:
+            break
+    return id
