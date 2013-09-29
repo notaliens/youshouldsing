10f79cf7f83bf785f57ec68429eaaea0356e6558
Tres Seaver <tseaver@palladion.com>
2013-09-28 22:19:30 -0400

    Get profile values saved / rendered.

--- src/yss/yss/views/login.py
+++ src/yss/yss/views/login.py
@@ -274,7 +274,7 @@ def authentication_type(request):
     renderer='templates/profile.pt',
 )
 def profile_view(context, request):
-    appstruct = {
+    return {
         'username': context.__name__,
         'display_name': getattr(context, 'display_name', ''),
         'email': getattr(context, 'email', ''),
@@ -282,12 +282,9 @@ def profile_view(context, request):
         'age': getattr(context, 'age', colander.null),
         'sex': getattr(context, 'sex', None),
         'favorite_genre': getattr(context, 'favorite_genre', None),
+        'form': None
     }
     form = deform.Form(YSSProfileSchema(), buttons=('Save',))
-    return {
-        'form': form.render(appstruct, readonly=True),
-    }
-
 
 @view_config(
     context=IUser,
@@ -295,17 +292,36 @@ def profile_view(context, request):
     name='edit.html',
 #    permission='edit', XXX
 )
-def profile_edit(context, request):
-    appstruct = {
-        'username': context.__name__,
-        'display_name': getattr(context, 'display_name', ''),
-        'email': getattr(context, 'email', ''),
-        'photo_url': getattr(context, 'photo_url', ''),
-        'age': getattr(context, 'age', colander.null),
-        'sex': getattr(context, 'sex', None),
-        'favorite_genre': getattr(context, 'favorite_genre', None),
-    }
-    form = deform.Form(YSSProfileSchema(), buttons=('Save',))
+def profile_edit_form(context, request):
+    schema = YSSProfileSchema().bind(request=request, context=context)
+    form = deform.Form(schema, buttons=('Save',))
+    rendered = None
+    if 'Save' in request.POST:
+        controls = request.POST.items()
+        try:
+            appstruct = form.validate(controls)
+        except deform.ValidationFailure as e:
+            rendered = e.render()
+        else:
+            context.display_name = appstruct['display_name']
+            context.email = appstruct['email']
+            context.photo_url = appstruct['photo_url']
+            context.age = appstruct['age']
+            context.sex = appstruct['sex']
+            context.favorite_genre = appstruct['favorite_genre']
+    else:
+        appstruct = {
+            'csrf_token': request.session.get_csrf_token(),
+            'username': context.__name__,
+            'display_name': getattr(context, 'display_name', ''),
+            'email': getattr(context, 'email', ''),
+            'photo_url': getattr(context, 'photo_url', ''),
+            'age': getattr(context, 'age', colander.null),
+            'sex': getattr(context, 'sex', None),
+            'favorite_genre': getattr(context, 'favorite_genre', None),
+        }
+    if rendered is None:
+        rendered = form.render(appstruct, readonly=False)
     return {
-        'form': form.render(appstruct, readonly=False),
+        'form': rendered,
     }
