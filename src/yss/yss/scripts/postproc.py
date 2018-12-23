8bd7aa1cb3c00992ad45651c9c161ad58cb503f1
Chris McDonough <chrism@plope.com>
2018-12-23 07:37:42 -0500

    recordings are now only as long as the mic audio

--- src/yss/yss/scripts/postproc.py
+++ src/yss/yss/scripts/postproc.py
@@ -16,6 +16,7 @@ from pyramid.traversal import find_resource
 from yss.utils import get_redis
 from yss.interfaces import framerate
 
+from io import StringIO
 
 def main(argv=sys.argv):
     def usage(msg):
@@ -59,20 +60,65 @@ def postprocess(recording):
         committed = recording.song.blob.committed()
         # change sample rate of mic audio to match the sample
         # rate of the source song XXX this probably should be done
-        # in javascript somehow
-        sox("-V", "--clobber", "audio.wav", "-r", "44100", "rerated.wav")
-        sox("-V", "--clobber", "-m", "rerated.wav", "-t", "mp3", "-v", "0.15",
-            committed, "mixed.wav")
-        ffmpeg("-y", "-i", "mixed.wav",
-               "-f", "image2", "-r", f"{framerate}", "-i", "frame%d.png",
-               "-acodec", "mp3", "video.mp4")
+        # in javascript somehow, but this is probably where we
+        # will apply reverb anyway, so the cost is already kinda sunk
+        err = StringIO()
+        sox(
+            "-V",
+            "--clobber",
+            "audio.wav",
+            "-r", "44100",
+            "rerated.wav",
+            _err=err,
+        )
+        # stderr will contain the duration that we can use to trim the file
+        # to its proper length in the next sox command, which does the
+        # mixing
+        samples = None
+        errlines = err.getvalue().split('\n')
+        for line in errlines:
+            if line.startswith('Duration'):
+                duration = line.split(':', 1)[1]
+                duration.strip()
+                duration, samples = duration.split('=', 1)
+                duration = duration.strip()
+                samples = samples.strip()
+                samples = samples.split(' ', 1)[0].strip()
+                samples = int(samples)
+                break
+        sox(
+            "-V",
+            "--clobber",
+            "-m", "rerated.wav",
+            "-t", "mp3",
+            "-v", "0.15",
+            committed,
+            "mixed.wav",
+            "trim" if samples is not None else "",
+            "0s" if samples is not None else "",
+            f"{samples}s" if samples is not None else "",
+        )
+        ffmpeg(
+            "-y",
+            "-i", "mixed.wav",
+            "-f", "image2",
+            "-r", f"{framerate}",
+            "-i", "frame%d.png",
+            "-acodec", "mp3",
+            "-shortest",
+            "video.mp4"
+        )
         recording.blob = Blob()
         with recording.blob.open("w") as saveto:
             with open("video.mp4", "rb") as savefrom:
                 shutil.copyfileobj(savefrom, saveto)
         print ("%s/%s" % (tmpdir, "video.mp4"))
-        #shutil.rmtree(tmpdir)
         transaction.commit()
+        # don't remove tempdir until commit succeeds
+        #shutil.rmtree(tmpdir)
+    except FileNotFoundError:
+        # no such file or dir when chdir
+        recording.postproc_failure = True # currently not exposed
     finally:
         os.chdir(curdir)
 
